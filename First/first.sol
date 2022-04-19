3d33b827ad1befb844bbec75fe7a940ae4e65b47
SecondPort <gomezmanulm@gmail.com>
2022-04-19 14:02:36 -0300

    first

---
+++ First/first.sol
@@ -0,0 +1,16 @@
+/// @title First contract
+/// @dev This contract is used to test the deployment of multiple contracts.
+/// @author SecondPort
+
+pragma solidity 0.8.13;  /* version */
+import "./ERC20.sol";
+
+contract First {
+
+    address owner;// variable que tiene la direccion de quien despliega el contrato
+    ERC20Basic token;
+    constructor() public{
+        owner = msg.sender; //dirreccion del remitente
+        token = new ERC20Basic(1000);// cantidad de tokens a desplegar
+    }
+}
---
+++ Propiedades de transacciones y bloques/funciones_globales.sol
@@ -0,0 +1,39 @@
+pragma solidity 0.8.13;
+
+contract funciones_globales{
+
+    //msg.sender
+    function getSender() public view returns(address){
+        return msg.sender;// retorna la direccion del remitente
+    }
+    // funcion now
+    function getNow() public view returns(uint256){
+        return block.timestamp;// retorna la fecha actual
+    }
+
+
+    //coinbase
+    function getCoinbase() public view returns(address){
+        return block.coinbase;// devuelve la direccion del minero que procesa el bloque
+    }
+
+    //block.difficulty
+    function getDifficulty() public view returns(uint256){
+        return block.difficulty;// devuelve la dificultad del bloque
+    }
+
+    //block number
+    function getBlockNumber() public view returns(uint256){
+        return block.number;// devuelve el numero del bloque
+    }
+
+    //msg.sig
+    function getSig() public view returns(bytes4){
+        return msg.sig;// devuelve el hash de la transaccion
+    }
+
+    //tx.gasprice
+    function getGasPrice() public view returns(uint256){
+        return tx.gasprice;// devuelve el precio del gas
+    }
+}
