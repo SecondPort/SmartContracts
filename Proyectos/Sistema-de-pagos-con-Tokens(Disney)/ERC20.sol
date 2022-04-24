65d6b09c70864f744c9065d0621d5298dbd48684
SecondPort <gomezmanulm@gmail.com>
2022-04-24 13:14:53 -0300

    changes5

--- Proyectos/Sistema-de-pagos-con-Tokens(Disney)/ERC20.sol
+++ Proyectos/Sistema-de-pagos-con-Tokens(Disney)/ERC20.sol
@@ -12,7 +12,7 @@ interface IERC20{
     function transferencia_disney(address sender, address recipient, uint256 amount) external returns (bool);
     function approve (address spender, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint amount) external returns (bool);
-    
+
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval (address indexed owner, address indexed spender, uint256 value);
 }
@@ -21,69 +21,66 @@ contract ERC20Basic is IERC20 {
     string public constant name = "ERC20Basic";
     string public constant symbol = "JBJ-TOKEN";
     uint8 public constant decimals = 2;
-    
-    event Transfer(address indexed from, address indexed to, uint256 tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-    
+
+    event Transfer(address indexed from, address indexed to, uint200 tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint240 tokenss);
+
     mapping (address => uint) balances;
     mapping(address => mapping (address => uint)) allowed;
     uint256 totalSupply_;
-    
+
     using SafeMath for uint256;
-    
-    constructor (uint256 total) public{
+
+    constructor (uint256 total) public {
         totalSupply_ = total;
         balances[msg.sender] = totalSupply_;
     }
-    
-    function totalSupply() public override view returns (uint256){
+
+    function totalSupply() public view returns (uint256){
         return totalSupply_;
     }
-    
+
     function increaseTotalSuply(uint newTokens) public{
         totalSupply_ += newTokens;
         balances[msg.sender] += newTokens;
     }
-    
-    function balanceOf (address tokenOwner) public override view returns (uint256){
+
+    function balanceOf (address tokenOwner) public view returns (uint256){
         return balances[tokenOwner];
     }
-    
-    function transfer(address receiver, uint256 numTokens) public override returns (bool){
-        require(numTokens <= balances[msg.sender]);
+
+    function transfer(address receiver, uint256 numTokens) public returns (bool){
+        require(numTokens <= balances[msg.sender],"You don't have enough tokens in your account");
         balances[msg.sender] = balances[msg.sender].sub(numTokens);
         balances[receiver] = balances[receiver].add(numTokens);
         emit Transfer(msg.sender,receiver,numTokens);
         return true;
-    } 
-    
-    function transferencia_disney(address sender, address receiver, uint256 numTokens) public override returns (bool){
-        require(numTokens <= balances[sender]);
+    }
+
+    function transferencia_disney(address sender, address receiver, uint256 numTokens) public returns (bool){
+        require(numTokens <= balances[sender],"You don't have enough tokens in your account");
         balances[sender] = balances[sender].sub(numTokens);
         balances[receiver] = balances[receiver].add(numTokens);
         emit Transfer(sender,receiver,numTokens);
         return true;
-    } 
-    
-    function approve (address delegate, uint256 numTokens) public override returns (bool) {
+    }
+
+    function approve (address delegate, uint256 numTokens) public returns (bool) {
         allowed[msg.sender][delegate] = numTokens;
         emit Approval(msg.sender, delegate, numTokens);
         return true;
     }
-    
-    function allowance (address owner, address delegate) public override view returns (uint){
+
+    function allowance (address owner, address delegate) public view returns (uint){
         return allowed[owner][delegate];
     }
-    
-    function transferFrom(address owner, address buyer, uint256 numTokens) public override returns (bool){
-        require (numTokens <= balances[owner]);
-        require (numTokens <= allowed[owner][msg.sender]);
-        
+    function transferFrom(address owner, address buyer, uint256 numTokens) public returns (bool){
+        require (numTokens <= balances[owner], "You don't have enough tokens in your account");
+        require (numTokens <= allowed[owner][msg.sender], "You don't have enough tokens in your account");
         balances[owner] = balances[owner].sub(numTokens);
         allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(numTokens);
         balances[buyer] = balances[buyer].add(numTokens);
         emit Transfer(owner,buyer,numTokens);
         return true;
     }
-    
 }
